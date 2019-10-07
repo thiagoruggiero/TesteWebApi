c651e93a72e91ab603d9e8facdb9c9ba03d85c33
Thiago Ruggiero <thiago.ruggiero@avenuecode.com>
2019-10-07 13:26:05 -0300

    incluindo rota post

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -48,5 +48,15 @@ namespace TesteWebApi.Controllers
 
             return todoItem;
         }
+
+        // POST: api/Todo
+        [HttpPost]
+        public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem item)
+        {
+            _context.TodoItems.Add(item);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetTodoItem), new { id = item.Id }, item);
+        }
     }
 }
