00002ba496a713b515fddf8c3b8089f75d5ccf25
Thiago Ruggiero <thiago.ruggiero@avenuecode.com>
2019-10-07 16:19:22 -0300

    Criando exemplo do github

---
+++ RestEaseTest/IGithubClient.cs
@@ -0,0 +1,18 @@
+﻿using RestEase;
+using RestEaseTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RestEaseTest
+{
+    [Header("User-Agent", "RestEase Sample")]
+    public interface IGithubClient
+    {
+        [Get("users/{username}")]
+        Task<GithubUser> GetUserAsync([Path("username")] string username,CancellationToken cancellationtoken);
+    }
+
+}
--- RestEaseTest/Program.cs
+++ RestEaseTest/Program.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using RestEase;
 
 namespace RestEaseTest
 {
@@ -14,7 +16,15 @@ namespace RestEaseTest
     {
         public static void Main(string[] args)
         {
+            MakeCallToGitHubAsync();
             CreateWebHostBuilder(args).Build().Run();
+        }
+
+        private static async void MakeCallToGitHubAsync()
+        {
+            var githubClient = RestClient.For<IGithubClient>("https://api.github.com");
+            var user = await githubClient.GetUserAsync("thiagoruggiero", CancellationToken.None);
+            Console.WriteLine(user.ReceivedEventsUrl);
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
